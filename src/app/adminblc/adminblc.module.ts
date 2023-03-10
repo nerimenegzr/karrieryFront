3231022022340a1c89c451b3bde037ddf46294be
nerimenegzr <125367495+nerimenegzr@users.noreply.github.com>
2023-03-10 11:48:26 +0500

    initial

---
+++ src/app/adminblc/adminblc.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminblcRoutingModule } from './adminblc-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AdminblcRoutingModule
+  ]
+})
+export class AdminblcModule { }
